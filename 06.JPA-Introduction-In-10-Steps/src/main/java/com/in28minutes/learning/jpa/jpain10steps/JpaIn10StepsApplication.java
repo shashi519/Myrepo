0889734f6a4c651bf7d035866f3d28fdc09f2401
shashi519 <miryalashashi@gmail.com>
2021-01-05 19:28:09 +0530

    Update JpaIn10StepsApplication.java

--- 06.JPA-Introduction-In-10-Steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/JpaIn10StepsApplication.java
+++ 06.JPA-Introduction-In-10-Steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/JpaIn10StepsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpaIn10StepsApplication {
-
+//test commit
 	public static void main(String[] args) {
 		SpringApplication.run(JpaIn10StepsApplication.class, args);
 	}
